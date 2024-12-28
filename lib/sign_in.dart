83c98cbc8928a6fbd47890e2eb891ebb648258c9
TerkumaAmos <agberterkuma2020@gmail.com>
2024-12-28 17:10:44 +0100

    new

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -10,27 +10,48 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color(0xFF1A1B35),
       body: Column(
         children: [
           Expanded(
-            flex: 2,
+            flex: 3,
             child: Stack(
               children: [
                 Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Center(
+                    const Center(
                       child: Text(
                         'Sign In',
                         style: TextStyle(
-                          color: Colors.yellow,
+                          color: Colors.orange,
                           fontSize: 24,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 60.0),
+                      child: Column(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange,
+                              minimumSize: Size(double.infinity, 60),
+                            ),
+                            child: const Text(
+                              "Sign In With Google",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ],
