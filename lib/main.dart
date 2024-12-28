e11b5d977efe8d9b78853086606894f7c5687770
TerkumaAmos <agberterkuma2020@gmail.com>
2024-12-28 18:34:24 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -26,13 +26,16 @@ class _MyAppState extends State<MyApp> {
               child: Stack(
                 children: [
                   // Add your illustration here using Image.asset
-                  Image.asset(
-                    'assets/web1.jpeg',
-                    color: Colors.blue.withOpacity(0.5),
-                    colorBlendMode: BlendMode.softLight,
-                    width: 500,
-                    height: 500,
-                    fit: BoxFit.fill,
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Image.asset(
+                      'assets/web1.jpeg',
+                      color: Colors.blue.withOpacity(0.5),
+                      colorBlendMode: BlendMode.softLight,
+                      width: 500,
+                      height: 500,
+                      fit: BoxFit.fill,
+                    ),
                   ),
                 ],
               ),
