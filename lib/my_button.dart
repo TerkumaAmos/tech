fdb17d0bc8a9cf34c13e813d12efd0c89322c628
TerkumaAmos <agberterkuma2020@gmail.com>
2025-01-02 08:33:44 +0100

    new

--- lib/my_button.dart
+++ lib/my_button.dart
@@ -8,17 +8,14 @@ class MyButton extends StatelessWidget {
     return Center(
       child: Material(
         elevation: 8.0,
-        borderRadius: BorderRadius.circular(12.0),
+        borderRadius: BorderRadius.circular(20.0),
         child: InkWell(
           child: Container(
             height: 50,
             width: 200,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
             child: const Row(
               children: [
-                Icon(Icons.question_answer),
+                Icon(Icons.question_mark),
                 SizedBox(
                   width: 8,
                 ),
