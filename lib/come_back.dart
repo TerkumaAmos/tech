d50c96ac76f8e7dfba8434b4ed7f13defe0b72c0
TerkumaAmos <agberterkuma2020@gmail.com>
2025-01-03 03:58:31 +0100

    new

--- lib/come_back.dart
+++ lib/come_back.dart
@@ -6,23 +6,30 @@ class ComeBack extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          CircleAvatar(
-            radius: 60,
-          ),
-          SizedBox(height: 8),
-          Text(
-            'Welcome Back',
-            style: TextStyle(color: Colors.blue, fontSize: 40, height: 1.0),
-          ),
-          SizedBox(height: 25),
-          Text(
-            'Flutter Fairy',
-            style: TextStyle(color: Colors.blue, fontSize: 40, height: 1.0),
-          )
-        ],
+      backgroundColor: Color(0xFF1A1B35), // Dark blue background
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 102,
+            ),
+            CircleAvatar(
+              radius: 60,
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Welcome Back',
+              style: TextStyle(color: Colors.blue, fontSize: 40, height: 1.0),
+            ),
+            SizedBox(height: 25),
+            Text(
+              'Flutter Fairy',
+              style: TextStyle(color: Colors.blue, fontSize: 40, height: 1.0),
+            )
+          ],
+        ),
       ),
     );
   }
