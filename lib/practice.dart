25a00d1142db5efc2a06677c2535873f1ae81b46
TerkumaAmos <agberterkuma2020@gmail.com>
2025-01-06 23:40:56 +0100

    new

--- lib/practice.dart
+++ lib/practice.dart
@@ -18,14 +18,47 @@ class _PracticeState extends State<Practice> {
           child: Column(
         children: [
           Text('Hello'),
-          AppTextField(
-            hintText: "sign Up",
-            controller: emaileditinggcontroller,
+          Center(
+            child: AppTextField(
+              hintText: "sign Up",
+              controller: emaileditinggcontroller,
+            ),
           ),
-          AppTextField(
-              hintText: "Password", controller: passwordeditingcontroller)
+          SizedBox(
+            height: 20,
+          ),
+          newPassword(passwordeditingcontroller: passwordeditingcontroller),
         ],
       )),
     );
+  }
+}
+
+class newPassword extends StatefulWidget {
+  const newPassword({
+    super.key,
+    required this.passwordeditingcontroller,
+  });
+
+  final TextEditingController passwordeditingcontroller;
+
+  @override
+  State<newPassword> createState() => _newPasswordState();
+}
+
+class _newPasswordState extends State<newPassword> {
+  bool _obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+    return AppTextField(
+        obscureText: _obscureText,
+        hintText: "Password",
+        controller: widget.passwordeditingcontroller,
+        suffixIcon: GestureDetector(
+          onTap: () {
+            _obscureText = true;
+          },
+          child: Icon(Icons.password_outlined),
+        ));
   }
 }
