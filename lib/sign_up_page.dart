347560e500e9538c9ef54324563dc2c1a51acbf6
TerkumaAmos <agberterkuma2020@gmail.com>
2025-01-06 21:17:55 +0100

    new

--- lib/sign_up_page.dart
+++ lib/sign_up_page.dart
@@ -1,29 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:tech/app_text.dart';
 
-void main() {
-  runApp(SignUpPage());
-}
-
-class SignUpPage extends StatelessWidget {
-  final passwordediting = TextEditingController;
-  final emailediting = TextEditingController;
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
 
   @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final textEditingController = TextEditingController();
+  final emailEditingController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Center(
-            child: AppTextField(
-              hintText: 'Password',
-              controller: TextEditingController(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Text('Sign Up'),
+            password(emailEditingController: emailEditingController),
+            const SizedBox(
+              height: 20,
             ),
-          ),
-          AppTextField(hintText: "Sign up", controller: TextEditingController())
-        ],
+            Center(
+              child: AppTextField(
+                hintText: 'Sign up',
+                controller: textEditingController,
+              ),
+            ),
+            // SizedBox(
+            //   height: 20,
+            // ),
+            // AppTextField(
+            //   controller: emailEditingController,
+            //   suffixIcon: const Icon(
+            //     Icons.email,
+            //     size: 100,
+            //   ),
+            // ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class password extends StatefulWidget {
+  password({
+    super.key,
+    required this.emailEditingController,
+  });
+
+  final TextEditingController emailEditingController;
+
+  @override
+  State<password> createState() => _passwordState();
+}
+
+class _passwordState extends State<password> {
+  bool _obscuretext = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppTextField(
+      obscureText: _obscuretext,
+      hintText: 'Password',
+      controller: widget.emailEditingController,
+      suffixIcon: GestureDetector(
+          onTap: () {
+            setState(() {
+              _obscuretext = !_obscuretext;
+            });
+          },
+          child: Icon(
+            Icons.password,
+          )),
     );
   }
 }
